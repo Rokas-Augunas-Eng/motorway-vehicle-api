462ce11b4db42ada0a219b761907d9ef2ab03722
rokas augunas <augunasr@gmail.com>
2021-09-26 20:56:37 +0100

    database connection set up

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Database
+const db = require("./config/sequelize.js");
+
+// Test DB
+db.authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.log("Error:" + err));
+
 app.get("/", (req, res) => res.send("vehicle_api_test"));
 
 const PORT = process.env.PORT || 3000;
